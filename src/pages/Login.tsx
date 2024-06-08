85ea9e809964c577c61681cd2b67133de9250af1
Chí Thành <chithanh171004@gmail.com>
2024-06-08 17:49:05 +0700

    Update css

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -82,63 +82,67 @@ export default function Login() {
           />
           {errors.email && <p>{errors.email.message?.toString()}</p>}
         </div>
-        <div className="relative">
-          <label htmlFor="password">Password</label>
-          <input
-            type={isShow1 ? "text" : "password"}
-            id="password"
-            placeholder="Example123@"
-            className={`${errors.password ? "border-2 border-red-600" : ""}`}
-            {...register("password", {
-              required: "This field is required",
-              pattern: {
-                value:
-                  /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>/?]).{11,}$/,
-                message:
-                  "Password must be longer than 10 characters and contain 1 uppercase character and 1 special character",
-              },
-            })}
-          />
-          {isShow1 ? (
-            <EyeOff
-              onClick={() => setIsShow1((is) => !is)}
-              className="absolute right-3 top-1/2 translate-y-1 cursor-pointer text-gray-400"
-            />
-          ) : (
-            <Eye
-              onClick={() => setIsShow1((is) => !is)}
-              className="absolute right-3 top-1/2 translate-y-1 cursor-pointer text-gray-400"
-            />
-          )}
-          {errors.password && <p>{errors.password.message?.toString()}</p>}
-        </div>
-        {!isLogin && (
+        <div>
           <div className="relative">
-            <label htmlFor="confirmPassword">Confirm password</label>
+            <label htmlFor="password">Password</label>
             <input
-              type={isShow2 ? "text" : "password"}
-              id="confirmPassword"
-              placeholder="***********"
-              className={`${
-                errors.confirmPassword ? "border-2 border-red-600" : ""
-              }`}
-              {...register("confirmPassword", {
-                required: "Please confirm your password again",
-                validate: (value: string) =>
-                  getValues().password === value || "Incorrect password",
+              type={isShow1 ? "text" : "password"}
+              id="password"
+              placeholder="Example123@"
+              className={`${errors.password ? "border-2 border-red-600" : ""}`}
+              {...register("password", {
+                required: "This field is required",
+                pattern: {
+                  value:
+                    /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>/?]).{11,}$/,
+                  message:
+                    "Password must be longer than 10 characters and contain 1 uppercase character and 1 special character",
+                },
               })}
             />
-            {isShow2 ? (
+            {isShow1 ? (
               <EyeOff
-                onClick={() => setIsShow2((is) => !is)}
+                onClick={() => setIsShow1((is) => !is)}
                 className="absolute right-3 top-1/2 translate-y-1 cursor-pointer text-gray-400"
               />
             ) : (
               <Eye
-                onClick={() => setIsShow2((is) => !is)}
+                onClick={() => setIsShow1((is) => !is)}
                 className="absolute right-3 top-1/2 translate-y-1 cursor-pointer text-gray-400"
               />
             )}
+          </div>
+          {errors.password && <p>{errors.password.message?.toString()}</p>}
+        </div>
+        {!isLogin && (
+          <div>
+            <div className="relative">
+              <label htmlFor="confirmPassword">Confirm password</label>
+              <input
+                type={isShow2 ? "text" : "password"}
+                id="confirmPassword"
+                placeholder="***********"
+                className={`${
+                  errors.confirmPassword ? "border-2 border-red-600" : ""
+                }`}
+                {...register("confirmPassword", {
+                  required: "Please confirm your password again",
+                  validate: (value: string) =>
+                    getValues().password === value || "Incorrect password",
+                })}
+              />
+              {isShow2 ? (
+                <EyeOff
+                  onClick={() => setIsShow2((is) => !is)}
+                  className="absolute right-3 top-1/2 translate-y-1 cursor-pointer text-gray-400"
+                />
+              ) : (
+                <Eye
+                  onClick={() => setIsShow2((is) => !is)}
+                  className="absolute right-3 top-1/2 translate-y-1 cursor-pointer text-gray-400"
+                />
+              )}
+            </div>
             {errors.confirmPassword && (
               <p>{errors.confirmPassword.message?.toString()}</p>
             )}
